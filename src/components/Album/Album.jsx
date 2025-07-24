9a873c2ffe989a3d7154095be69704315382884c
Bhargav Patel <bhargavrpatelcanada@gmail.com>
2025-07-24 09:54:49 -0400

    Final project

---
+++ src/components/Album/Album.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import './Album.css'
+import ListOfSong from './ListOfSong';
+
+const Album = () => {
+
+    const [topAlbum, setTopAlbum] = useState([]);
+    const [newAlbum, setNewAlbum] = useState([]);
+    const [song, setSong] = useState([])
+
+    useEffect(() => {
+        fetchTheAlbum()
+    }, [])
+
+    const fetchTheAlbum = async () => {
+        try {
+            let response = await fetch('https://qtify-backend-labs.crio.do/albums/top')
+            let data = await response.json()
+            setTopAlbum(data)
+
+            response = await fetch('https://qtify-backend-labs.crio.do/albums/new')
+            data = await response.json()
+            setNewAlbum(data)
+
+            response = await fetch('https://qtify-backend-labs.crio.do/songs')
+            data = await response.json()
+            setSong(data)
+        } catch (error) {
+            console.log("Error while fetching the albumb",error)
+        }
+    }
+    return (
+        <div className='album'>
+            {/* {topAlbum.map((al, index) => ( */}
+                <ListOfSong album={topAlbum[0]?.songs} name={'Top Album'}/>
+                <ListOfSong album={newAlbum[0]?.songs} name={'New Album'}/>
+                <ListOfSong album={song} name={'Songs'} />
+            {/* ))} */}
+        </div>
+    )
+}
+
+export default Album
