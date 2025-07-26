6795476db200b990a89fcfb14c080ed80535d93a
Bhargav Patel <bhargavrpatelcanada@gmail.com>
2025-07-26 16:30:09 -0400

    test 8

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import logo from '../../assets/logo.png'
-import './Logo.css'
+// Logo.jsx
+import React from 'react';
+import logo from '../../assets/logo.png';
+import './Logo.css';
 
 const Logo = () => {
   return (
-    // <div className='header-logo' data-testid="logo">
-         <img className='image-logo' src={logo} alt='logo' />
-    // </div>
-  )
-}
+    <div className="header-logo" data-testid="logo">
+      <img className="image-logo" src={logo} alt="logo" />
+    </div>
+  );
+};
 
-export default Logo
+export default Logo;
